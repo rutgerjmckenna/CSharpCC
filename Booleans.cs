356bf0f8fb3e37425fb1fac4d06adf02d2edc554
Rutger McKenna <44029087+rutgerjmckenna@users.noreply.github.com>
2020-04-16 17:13:40 -0400

    booleans

---
+++ Booleans.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BooleanDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          bool answerOne = true;
+          bool answerTwo = false;
+        }
+    }
+}
