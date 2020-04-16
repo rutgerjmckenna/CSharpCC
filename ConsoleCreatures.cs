8d6447888ab113c1027e8770ac00130aea86418a
Rutger McKenna <44029087+rutgerjmckenna@users.noreply.github.com>
2020-04-16 17:01:45 -0400

    Console Creatures

---
+++ ConsoleCreatures.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Gunna make a silly face, dudes!
+      Console.WriteLine(" .-.");    
+      Console.WriteLine("(o o)");    
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Hi, I'm Spooky Sally!");
+     }
+  }
+}
