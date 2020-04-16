a53bdda9cb3caf47862fac2ed49f64173b9e130a
Rutger McKenna <44029087+rutgerjmckenna@users.noreply.github.com>
2020-04-16 17:11:04 -0400

    String Parts

---
+++ StringParts.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace NameGrab
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // User Name
+      string name = "Farhad Hesam Abbasi";
+
+      // Get first letter
+      int charPosition = name.IndexOf("F");
+      char firstLetter = name[charPosition];
+
+      // Get last name
+      charPosition = name.IndexOf("Abbasi");
+      string lastName = name.Substring(charPosition);
+
+      // Print results
+      Console.WriteLine($"{firstLetter}. {lastName}");
+
+    }
+  }
+}
